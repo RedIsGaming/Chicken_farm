bf9240c8ad619d4ce83bc863cd991289fab4ea9a
RedIsGaming <jaredhofman@protonmail.com>
2023-05-10 23:26:08 +0200

    fix: DC and MCDC tests and doc

--- src/main/test/DC.java
+++ src/main/test/DC.java
@@ -12,25 +12,13 @@ public class DC {
     */
     @Test
     public void checkFarm1() {
-        boolean result = Farm.checkFarm(false, 762,"Elephant");
-        assertFalse(result);
-    }
-
-    @Test
-    public void checkFarm2() {
         boolean result = Farm.checkFarm(false, 349, "White Cochin");
         assertTrue(result);
     }
 
     @Test
-    public void checkFarm3() {
+    public void checkFarm2() {
         boolean result = Farm.checkFarm(true, -60, "Cow");
         assertFalse(result);
-    }
-
-    @Test
-    public void checkFarm4() {
-        boolean result = Farm.checkFarm(true,1200, "Rhode Island Red");
-        assertTrue(result);
     }
 }
--- src/main/test/MCDC.java
+++ src/main/test/MCDC.java
@@ -12,37 +12,37 @@ public class MCDC {
     */
     @Test
     public void checkFarm1() {
-        boolean result = Farm.checkFarm(false, 762,"Flamingo");
-        assertFalse(result);
-    }
-
-    @Test
-    public void checkFarm2() {
         boolean result = Farm.checkFarm(false, 80, "Crocodile");
         assertFalse(result);
     }
 
     @Test
-    public void checkFarm3() {
+    public void checkFarm2() {
         boolean result = Farm.checkFarm(false, -92, "Black Sex Link");
         assertTrue(result);
     }
 
     @Test
-    public void checkFarm4() {
-        boolean result = Farm.checkFarm(true,1200, "Lion");
-        assertFalse(result);
+    public void checkFarm3() {
+        boolean result = Farm.checkFarm(false, 760,"White Cochin");
+        assertTrue(result);
     }
 
     @Test
-    public void checkFarm5() {
+    public void checkFarm4() {
         boolean result = Farm.checkFarm(true,740, "Giraffe");
         assertTrue(result);
     }
 
     @Test
-    public void checkFarm6() {
+    public void checkFarm5() {
         boolean result = Farm.checkFarm(true,5475, "Buff Orpington");
+        assertTrue(result);
+    }
+
+    @Test
+    public void checkFarm6() {
+        boolean result = Farm.checkFarm(true,1000, "Buff Laced Polish");
         assertTrue(result);
     }
 }
